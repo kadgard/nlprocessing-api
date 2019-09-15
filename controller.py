787a648c4e73cefe7a1287a326aaea048a8e82e9
Daniel Machado <daniel.machado@dtidigital.com.br>
2019-09-15 12:19:27 -0300

    insert error handler in flask controller

--- controller.py
+++ controller.py
@@ -1,11 +1,32 @@
 from app import app
+from flask import jsonify
 from flask import request
-
-
 
 @app.route('/', methods=['POST'])
 def process_text():
     return 'Foi minunu!'
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.errorhandler(400)
+def bad_request(error=None):
+    message = {
+        'status': 400,
+        'message': 'Bad Request: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
+
+    return resp
 
 if __name__ == "__main__":
     app.run()
